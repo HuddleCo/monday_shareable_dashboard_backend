fc6794615d49af8835a6fc9f3169fb293a53620b
David Milanese <milanese.david@gmail.com>
2021-10-01 20:44:47 +0930

    chore: refactor share controller. prefer using promises

--- src/controllers/shareController.js
+++ src/controllers/shareController.js
@@ -1,12 +1,13 @@
 import fs from "fs";
 
-const readFile = (path) => fs.readFileSync(path, {
+const FILE_OPTIONS = {
   encoding: "utf8",
   flag: "r",
-});
-
-const shareController = (req, res) => {
-  res.send(readFile(req.query?.filename || "./file.html"));
 };
+
+const shareController = (req, res) =>
+  fs.promises.readFile(req.query?.filename, FILE_OPTIONS)
+    .then((html) => res.send(html))
+    .catch((error) => res.status(404).send(`<p>File not found ${error.message}</p>`))
 
 export { shareController };
