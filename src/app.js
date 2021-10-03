195a21a1f0829c4d0f97b4f8d63c178f0bacd967
David Milanese <milanese.david@gmail.com>
2021-10-03 22:40:03 +1030

    feat: install logger for web requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 import { getDashboardController } from "./controllers/dashboardController.js";
 import { shareController } from "./controllers/shareController.js";
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8080
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(cors({ origin: true }));
 app.use(express.json());
 
