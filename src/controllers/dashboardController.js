625ed240ad9e55e1b8c9b9cc73326ec78d91089e
David Milanese <milanese.david@gmail.com>
2021-10-07 12:00:18 +1030

    chore: rename constants

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -2,11 +2,12 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import tmp from 'tmp';
 
-const usernameSelector = '#user_email';
-const passwordSelector = '#user_password';
-const submitSelector = '#login-monday-container > div > div.router-wrapper > div > div.email-page > div > div.next-button-wrapper > div > button';
-const dotSelector = '#first-level-content > div > div > div.overview-header.board-header > div.overview-header-content-wrapper > div > div.overview-header-right > div > div > div.overview-menu > div > span > button';
-const tvModeSelector = '#first-level-content > div.dialog-node > div > div > div > div.ds-menu-inner > div:nth-child(1)';
+const USERNAME_SELECTOR = '#user_email';
+const PASSWORD_SELECTOR = '#user_password';
+const SUBMIT_SELECTOR = '#login-monday-container > div > div.router-wrapper > div > div.email-page > div > div.next-button-wrapper > div > button';
+const MENU_SELECTOR = '#first-level-content > div > div > div.overview-header.board-header > div.overview-header-content-wrapper > div > div.overview-header-right > div > div > div.overview-menu > div > span > button';
+const TV_MODE_SELECTOR = '#first-level-content > div.dialog-node > div > div > div > div.ds-menu-inner > div:nth-child(1)';
+const CLICK_TIMEOUT = 2000;
 
 async function startBrowser() {
   const browser = await puppeteer.launch({
@@ -26,20 +27,20 @@ async function closeBrowser(browser) {
 }
 
 async function login(page, username, password) {
-  await page.click(usernameSelector);
+  await page.click(USERNAME_SELECTOR);
   await page.keyboard.type(username);
-  await page.click(passwordSelector);
+  await page.click(PASSWORD_SELECTOR);
   await page.keyboard.type(password);
-  await page.click(submitSelector);
+  await page.click(SUBMIT_SELECTOR);
   await page.waitForNavigation();
 }
 
 async function getDashboard(page) {
-  await page.waitForSelector(dotSelector);
-  await page.click(dotSelector);
-  await page.waitForTimeout(2000);
-  await page.click(tvModeSelector);
-  await page.waitForTimeout(2000);
+  await page.waitForSelector(MENU_SELECTOR);
+  await page.click(MENU_SELECTOR);
+  await page.waitForTimeout(CLICK_TIMEOUT);
+  await page.click(TV_MODE_SELECTOR);
+  await page.waitForTimeout(CLICK_TIMEOUT);
   const html = await page.content();
 
   const tmpobj = tmp.fileSync({
